2c723daa03974f2d64d50c70937e77554d5ed97e
ErodeesFleurs <862959461@qq.com>
2026-02-04 09:19:12 +0800

    Add Nix flake and direnv support

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Rust devShell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
+          buildInputs = with pkgs; [
+            openssl
+            pkg-config
+            clang
+            cmake
+          ];
+
+          env = {
+          };
+        };
+
+      }
+    );
+}
