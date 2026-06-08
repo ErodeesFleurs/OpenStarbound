2761a974f10d3bb7ea3ebc2a97c326aad3cad2eb
ErodeesFleurs <862959461@qq.com>
2026-06-08 17:26:14 +0800

    Add imported targets for platform SDKs

--- cmake/FindDiscordApi.cmake
+++ cmake/FindDiscordApi.cmake
@@ -1,11 +1,25 @@
 # Variables defined by this module:
 #
 #  DISCORD_API_LIBRARY           The discord api library
+#  DiscordApi::DiscordApi        The discord api imported target
+
+include(FindPackageHandleStandardArgs)
 
 find_library(DISCORD_API_LIBRARY
-    NAMES discord_game_sdk libdiscord_game_sdk
+  NAMES discord_game_sdk libdiscord_game_sdk
 )
 
+find_package_handle_standard_args(DiscordApi
+  REQUIRED_VARS DISCORD_API_LIBRARY
+)
+
+if(DiscordApi_FOUND AND NOT TARGET DiscordApi::DiscordApi)
+  add_library(DiscordApi::DiscordApi UNKNOWN IMPORTED)
+  set_target_properties(DiscordApi::DiscordApi PROPERTIES
+    IMPORTED_LOCATION "${DISCORD_API_LIBRARY}"
+  )
+endif()
+
 mark_as_advanced(
-    DISCORD_API_LIBRARY
+  DISCORD_API_LIBRARY
 )
