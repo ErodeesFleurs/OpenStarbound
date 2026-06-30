53b9d46b60cc1dfdb7467ba91e4f1c7b7ba69bae
ErodeesFleurs <862959461@qq.com>
2026-06-30 13:52:46 +0800

    Modernize CMake target sources and object libraries
    Squashes 48 original commits from 9dd42b29..df2a0ffe.
    First: Use lowercase utility configure function
    Last:  Update gitignore
    
    Representative commits:
    - 9dd42b29 Use lowercase utility configure function
    - 1c7826bc Use target sources for rendering target
    - 73dcaa39 Use target sources for base target
    - 3a3c47a4 Model base dependency on core objects
    - 24384ad7 Model game dependency on base objects
    - df2a0ffe Update gitignore

--- cmake/FindGLEW.cmake
+++ cmake/FindGLEW.cmake
@@ -22,46 +22,60 @@
 # OTHER DEALINGS IN THE SOFTWARE.
 #
 #
-# Try to find GLEW library and include pathi. Once done this will define:
+# Try to find GLEW library and include path. Once done this will define:
 # GLEW_FOUND
 # GLEW_INCLUDE_DIR
+# GLEW_INCLUDE_DIRS
 # GLEW_LIBRARY
+# GLEW_LIBRARIES
+# GLEW::GLEW
 
-IF (WIN32)
-    FIND_PATH( GLEW_INCLUDE_DIR GL/glew.h
-        $ENV{PROGRAMFILES}/GLEW/include
-        ${PROJECT_SOURCE_DIR}/src/nvgl/glew/include
-        DOC "The directory where GL/glew.h resides")
-    FIND_LIBRARY( GLEW_LIBRARY
-        NAMES glew GLEW glew32 glew32s
-        PATHS
-        $ENV{PROGRAMFILES}/GLEW/lib
-        ${PROJECT_SOURCE_DIR}/src/nvgl/glew/bin
-        ${PROJECT_SOURCE_DIR}/src/nvgl/glew/lib
-        DOC "The GLEW library")
-ELSE (WIN32)
-    FIND_PATH( GLEW_INCLUDE_DIR GL/glew.h
-        /usr/include
-        /usr/local/include
-        /sw/include
-        /opt/local/include
-        DOC "The directory where GL/glew.h resides")
-    FIND_LIBRARY( GLEW_LIBRARY
-        NAMES GLEW glew
-        PATHS
-        /usr/lib64
-        /usr/lib
-        /usr/local/lib64
-        /usr/local/lib
-        /sw/lib
-        /opt/local/lib
-        DOC "The GLEW library")
-ENDIF (WIN32)
+include(FindPackageHandleStandardArgs)
 
-IF (GLEW_INCLUDE_DIR)
-    SET( GLEW_FOUND 1 CACHE STRING "Set to 1 if GLEW is found, 0 otherwise")
-ELSE (GLEW_INCLUDE_DIR)
-    SET( GLEW_FOUND 0 CACHE STRING "Set to 1 if GLEW is found, 0 otherwise")
-ENDIF (GLEW_INCLUDE_DIR)
+if(WIN32)
+  find_path(GLEW_INCLUDE_DIR GL/glew.h
+    $ENV{PROGRAMFILES}/GLEW/include
+    ${PROJECT_SOURCE_DIR}/src/nvgl/glew/include
+    DOC "The directory where GL/glew.h resides")
+  find_library(GLEW_LIBRARY
+    NAMES glew GLEW glew32 glew32s
+    PATHS
+    $ENV{PROGRAMFILES}/GLEW/lib
+    ${PROJECT_SOURCE_DIR}/src/nvgl/glew/bin
+    ${PROJECT_SOURCE_DIR}/src/nvgl/glew/lib
+    DOC "The GLEW library")
+else()
+  find_path(GLEW_INCLUDE_DIR GL/glew.h
+    /usr/include
+    /usr/local/include
+    /sw/include
+    /opt/local/include
+    DOC "The directory where GL/glew.h resides")
+  find_library(GLEW_LIBRARY
+    NAMES GLEW glew
+    PATHS
+    /usr/lib64
+    /usr/lib
+    /usr/local/lib64
+    /usr/local/lib
+    /sw/lib
+    /opt/local/lib
+    DOC "The GLEW library")
+endif()
 
-MARK_AS_ADVANCED( GLEW_FOUND )
+set(GLEW_INCLUDE_DIRS ${GLEW_INCLUDE_DIR})
+set(GLEW_LIBRARIES ${GLEW_LIBRARY})
+
+find_package_handle_standard_args(GLEW
+  REQUIRED_VARS GLEW_INCLUDE_DIR GLEW_LIBRARY
+)
+
+if(GLEW_FOUND AND NOT TARGET GLEW::GLEW)
+  add_library(GLEW::GLEW UNKNOWN IMPORTED)
+  set_target_properties(GLEW::GLEW PROPERTIES
+    IMPORTED_LOCATION "${GLEW_LIBRARY}"
+    INTERFACE_INCLUDE_DIRECTORIES "${GLEW_INCLUDE_DIRS}"
+  )
+endif()
+
+mark_as_advanced(GLEW_INCLUDE_DIR GLEW_LIBRARY)
--- cmake/FindSteamApi.cmake
+++ cmake/FindSteamApi.cmake
@@ -2,6 +2,9 @@
 #
 #  STEAM_API_LIBRARY           The steam api library
 #  STEAM_API_INCLUDE_DIR       The location of steam api headers
+#  SteamApi::SteamApi          The steam api imported target
+
+include(FindPackageHandleStandardArgs)
 
 find_library(STEAM_API_LIBRARY
   NAMES libsteam_api steam_api steam_api64
@@ -10,7 +13,19 @@ find_path(STEAM_API_INCLUDE_DIR
   steam/steam_api.h
 )
 
+find_package_handle_standard_args(SteamApi
+  REQUIRED_VARS STEAM_API_LIBRARY STEAM_API_INCLUDE_DIR
+)
+
+if(SteamApi_FOUND AND NOT TARGET SteamApi::SteamApi)
+  add_library(SteamApi::SteamApi UNKNOWN IMPORTED)
+  set_target_properties(SteamApi::SteamApi PROPERTIES
+    IMPORTED_LOCATION "${STEAM_API_LIBRARY}"
+    INTERFACE_INCLUDE_DIRECTORIES "${STEAM_API_INCLUDE_DIR}"
+  )
+endif()
+
 mark_as_advanced(
-    STEAM_API_LIBRARY
-    STEAM_API_INCLUDE_DIR
+  STEAM_API_LIBRARY
+  STEAM_API_INCLUDE_DIR
 )
